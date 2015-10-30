dfd371759dd2a358a414ba2c5b2fba2fa78bf806
SirSeim <sirseim@gmail.com>
2015-10-29 23:08:29 -0700

    more progress on twitch

--- front-end/content.js
+++ front-end/content.js
@@ -1,3 +1,40 @@
 $(function() {
-    alert("Hey");
+    // alert("Hey");
+    window.CLIENT_ID = 'n5t974xr5w5tw69lv8roafnqmmh49wt';
+    Twitch.init({clientId: CLIENT_ID}, function(error, status) {
+        if (status.authenticated) {
+            // we're logged in :)
+            $('#status').text('Logged in! Allowed scope: ' + status.scope);
+            // Show the data for logged-in users
+        } else {
+            $('#status').text('Not Logged in! Better connect with Twitch!');
+            // Show the twitch connect button
+        }
+    });
+
+    $('#twitch-connect').click(function() {
+        Twitch.login({
+            scope: ['user_read', 'channel_read']
+        });
+    });
+
+
+
+    window.onPlayerEvent = function (data) {
+        data.forEach(function(event) {
+            if (event.event == "playerInit") {
+                var player = $("#twitch_embed_player")[0];
+                player.playVideo();
+                player.mute();
+            }
+        });
+    }
+
+    swfobject.embedSWF("//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf", "twitch_embed_player", "640", "400", "11", null,
+        { "eventsCallback":"onPlayerEvent",
+            "embed":1,
+            "channel":"day9tv",
+            "auto_play":"true"},
+            { "allowScriptAccess":"always",
+                "allowFullScreen":"true"});
 });
